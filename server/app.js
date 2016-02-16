65188dc6532050782f88d13619428abed6b4036c
Ubuntu <ubuntu@ip-172-31-34-217.us-west-2.compute.internal>
2016-02-16 22:02:05 +0000

    Built dashboard from commit bf7c585 on branch master

---
+++ server/app.js
@@ -0,0 +1,51 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
+var ExpressStormpath = require('express-stormpath');
+var path = require('path');
+
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+	}
+);
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
+
+// Setup server
+var app = express();
+
+app.use(ExpressStormpath.init(app,{
+  website: true,
+  expand: {
+    groups: true
+  },
+  web: {
+    spaRoot: path.join(__dirname, '..','client','index.html')
+  }
+}));
+
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+
+app.on('stormpath.ready', function() {
+  // Start server
+  server.listen(config.port, config.ip, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
+});
+
+// Expose app
+exports = module.exports = app;
