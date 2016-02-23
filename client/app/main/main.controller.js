b2081fdcae114b39bb2b538ff7dad694c73ced7f
Ubuntu <ubuntu@ip-172-31-18-50.us-west-2.compute.internal>
2016-02-23 20:03:11 +0000

    Adding config for server side api access

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,7 +4,7 @@ angular.module('dashboardApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
+    $http.get(STORMPATH_CONFIG.ENDPOINT_PREFIX + '/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
 
@@ -12,11 +12,11 @@ angular.module('dashboardApp')
       if($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $http.post(STORMPATH_CONFIG.ENDPOINT_PREFIX + '/api/things', { name: $scope.newThing });
       $scope.newThing = '';
     };
 
     $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+      $http.delete(STORMPATH_CONFIG.ENDPOINT_PREFIX + '/api/things/' + thing._id);
     };
   });
