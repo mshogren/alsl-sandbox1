20c79ec64f0e9d57ec727393d1cefef3530d0a9d
Ubuntu <ubuntu@ip-172-31-18-50.us-west-2.compute.internal>
2016-02-28 21:06:06 +0000

    Add interceptor for api requests

--- client/app/app.js
+++ client/app/app.js
@@ -19,6 +19,19 @@ angular.module('dashboardApp', [
   .config(function(STORMPATH_CONFIG, ENV) {
     STORMPATH_CONFIG.ENDPOINT_PREFIX = ENV.apiEndpoint;
   })
+  .factory('ApiInterceptor', function(ENV) {
+    return {
+      'request': function(config) {
+        if(config.url[0] === '/') {
+          config.url = ENV.apiEndpoint + config.url;
+        }
+        return config;
+      }
+    };
+  })
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push('ApiInterceptor');
+  })
   .run(function($stormpath){
     $stormpath.uiRouter({
       loginState: 'login',
--- client/app/thing/thing.controller.spec.js
+++ client/app/thing/thing.controller.spec.js
@@ -11,10 +11,10 @@ describe('Controller: ThingCtrl', function () {
       $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope, STORMPATH_CONFIG) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET(STORMPATH_CONFIG.ENDPOINT_PREFIX + '/me').respond({});
-    $httpBackend.expectGET(STORMPATH_CONFIG.ENDPOINT_PREFIX + '/api/things')
+    $httpBackend.expectGET('/me').respond({});
+    $httpBackend.expectGET('/api/things')
       .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
     scope = $rootScope.$new();
